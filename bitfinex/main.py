e0d2252dc9b5af237ce49c67a76c1c59a9493664
nateGeorge <wordsforthewise@gmail.com>
2019-08-22 01:11:38 -0600

    update code to work with latest API and change method to be more robust

--- bitfinex/main.py
+++ bitfinex/main.py
@@ -4,6 +4,7 @@ import time
 
 import click
 import pendulum
+import pandas as pd
 
 from db import SqliteDatabase
 from utils import date_range, get_data
@@ -32,30 +33,37 @@ def symbol_start_date(symbol):
 
 def get_symbols():
     """
-    Return the symbols that are being traded on Bitfinex.
+    curl https://api-pub.bitfinex.com/v2/tickers?symbols=ALL
 
-    This is taken from APIv1 as v2 doesn't have this endpoint.
-    https://bitfinex.readme.io/v1/reference#rest-public-symbols
-
-    Note: APIv1 returns symbol names in lowercase ('btcusd') but
-    APIv2 endpoints require them in uppercase ('BTCUSD')
+    Transforms from tBTCUSD to btcusd which is due to the original code in this repo.
     """
     with open('symbols.json') as f:
         return json.load(f)
+    url = 'https://api-pub.bitfinex.com/v2/tickers?symbols=ALL'
+    data = get_data(url)
+    df = pd.DataFrame(data)
+    pair_df = df[df[0].str.contains('t\w\w\w\w\w\w')]
+    pair_df[0] = pair_df[0].apply(lambda x: x[1:].lower)
+    return pair_df[0].tolist()
 
 
-def get_candles(symbol, start_date, end_date, timeframe='1m', limit=1000):
+
+def get_candles(symbol, start_date, end_date, timeframe='5m', limit=1000, get_earliest=False):
     """
     Return symbol candles between two dates.
     https://docs.bitfinex.com/v2/reference#rest-public-candles
     """
-    # timestamps need to include milliseconds
-    start_date = start_date.int_timestamp * 1000
-    end_date = end_date.int_timestamp * 1000
+    if get_earliest:
+        url = f'{API_URL}/candles/trade:{timeframe}:t{symbol.upper()}/hist' \
+              f'?start=0&limit={limit}&sort=1'
+        data = get_data(url)
+        # reverse data
+        data = data[::-1]
+    else:
+        url = f'{API_URL}/candles/trade:{timeframe}:t{symbol.upper()}/hist' \
+              f'?start={start_date}&end={end_date}&limit={limit}'
+        data = get_data(url)
 
-    url = f'{API_URL}/candles/trade:{timeframe}:t{symbol.upper()}/hist' \
-          f'?start={start_date}&end={end_date}&limit={limit}'
-    data = get_data(url)
     return data
 
 
@@ -68,8 +76,6 @@ def main(db_path, debug):
         logger.setLevel(logging.DEBUG)
 
     db = SqliteDatabase(path=db_path)
-    end_date = pendulum.now()
-    step = pendulum.Interval(minutes=1000)
 
     symbols = get_symbols()
     logging.info(f'Found {len(symbols)} symbols')
@@ -80,24 +86,49 @@ def main(db_path, debug):
         latest_candle_date = db.get_latest_candle_date(symbol)
         if latest_candle_date is None:
             logging.debug('No previous entries in db. Starting from scratch')
-            # TODO: handle case when symbol is missing from trading start days
-            # e.g. symbol is in symbols.json but not in symbols_trading_start_days.json
-            start_date = symbol_start_date(symbol)
+            get_earliest = True
+            start_date = 0
+            logging.info(f'{i}/{len(symbols)} | {symbol} | Processing from beginning')
         else:
             logging.debug('Found previous db entries. Resuming from latest')
+            get_earliest = False
             start_date = latest_candle_date
+            logging.info(f'{i}/{len(symbols)} | {symbol} | Processing from {pd.to_datetime(start_date, unit="ms", utc=True)}')
 
-        logging.info(f'{i}/{len(symbols)} | {symbol} | Processing from {start_date.to_datetime_string()}')
-        for d1, d2 in date_range(start_date, end_date, step):
-            logging.debug(f'{d1} -> {d2}')
+        while True:
+            # add 5000 minutes in ms to the start date.
+            # bitfinex is supposed to return 5000 datapoints but always returns fewer
+            # probably due to not all bars having trades
+            end_date = start_date + 1000 * 5 * 60 * 1000
+            logging.debug(f'{start_date} -> {end_date}')
             # returns (max) 1000 candles, one for every minute
-            candles = get_candles(symbol, d1, d2)
+            if get_earliest:
+                candles = get_candles(symbol, start_date, end_date, get_earliest=True)
+                get_earliest = False
+            else:
+                candles = get_candles(symbol, start_date, end_date)
+
+            # df = pd.DataFrame(candles)
+            # time_diffs = df[0].astype('int').diff().value_counts()
+            # if len(time_diffs) > 1:
+            #     logging.debug('WARNING: more than one time difference:')
+            #     logging.debug(time_diffs)
+
+            # end when we don't see any new data
+            last_start_date = start_date
+            start_date = candles[0][0]
+
+            # seems like this modifies the original 'candles' to insert the ticker
             logging.debug(f'Fetched {len(candles)} candles')
             if candles:
                 db.insert_candles(symbol, candles)
 
-            # prevent from api rate-limiting
-            time.sleep(3)
+            if start_date == last_start_date:
+                break
+
+            # prevent from api rate-limiting -- 60 per minute claimed, but seems to be a little slower
+            time.sleep(2)
+
     db.close()
 
 
